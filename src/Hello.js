7907a42d8d18086e7165ac3a601052da2fb5a82b
yyyey <dmsdud322@naver.com>
2020-11-18 11:33:44 +0900

    begin-react1

---
+++ src/Hello.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Hello({name, color, isSpecial}) {
+    return (
+        <div style={{ color }}>
+            { isSpecial && <b>*</b> } 
+            안녕하세요 { name }
+        </div>
+    );
+}
+
+Hello.defaultProps = {
+    name: '이름없음'
+}
+
+export default Hello;
